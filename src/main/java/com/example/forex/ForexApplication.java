c0e4062e655c812c21791553f21fbcdd2ecbc2f9
Sayli-Karnik <karniksayli1995@gmail.com>
2017-07-11 06:08:17 -0700

    First commit

---
+++ src/main/java/com/example/forex/ForexApplication.java
@@ -0,0 +1,12 @@
+package com.example.forex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexApplication.class, args);
+	}
+}
