a1f7bba9e0f59fc25d29566d0c6f9eddc6592475
Sayli-Karnik <karniksayli1995@gmail.com>
2017-07-13 04:34:34 -0700

    4 user story

--- src/main/java/com/example/forex/TransactionOrderRepository.java
+++ src/main/java/com/example/forex/TransactionOrderRepository.java
@@ -1,9 +1,12 @@
+
+		
 package com.example.forex;
 
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -28,19 +31,7 @@ public class TransactionOrderRepository {
 			return jdbcTemplate.query("SELECT * FROM ORDERS", new OrderRowMapper());
 		}
 	    
-	    @Transactional
-	    public TransactionOrder saveOrder(TransactionOrder order) {
-	    	String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-	    	
-if(user_exists(order.getUsername(),order.getPassword()))
-	    	jdbcTemplate.update("insert into ORDERS(USERNAME, ORDER_TYPE, PRICE, QTY, PAIR, SIDE, TIME_STAMP,) values (?,?,?,?,?,?,?)", 
-	    order.getUsername(), order.getOrder_type(), order.getPrice(), order.getQuantity(),order.getPair(), order.getSide(), timeStamp);
-	        
-	   return order;
-	    }
-	    
-	    
-	    @Transactional
+		@Transactional
 		public User addUser(User username) {
 			
 			System.out.println("Password must satisfy following criteria:");
@@ -63,7 +54,115 @@ if(user_exists(order.getUsername(),order.getPassword()))
 			return username;
 		}
 	    
-	    
+		
+		@Transactional
+	    public TransactionOrder saveOrder(TransactionOrder order) {
+	    	String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+	    	
+	    	  String type=order.getSide();
+	    	  String query;
+	    	  String order_type=order.getOrder_type();
+	    	  System.out.println("Type is " + type);
+	          String other_type;
+	          String buyer = null;
+	          String seller=null;
+	            if(type.equals("BUY")){
+	            	System.out.println("In type BUY");
+	                buyer=order.getUsername();
+	                other_type="SELL";
+	            }
+	            else{
+	            	System.out.println("In type SELL");
+	                seller=order.getUsername();
+	                other_type="BUY";
+	            }
+
+	            double price=order.getPrice();
+	            System.out.println("Seller is " + seller);
+	            String pair=order.getPair();
+	            int quantity=order.getQuantity();
+	            
+	            List<OpenClosedOrders> list3 = new ArrayList<OpenClosedOrders>();
+	            String sql3="select PRICE from SUC_TRANSACTION where PAIR= '" +order.getPair()+ "' order by TID DESC";
+	            list3=jdbcTemplate.query(sql3,new UserRow());
+	            double market_price=list3.get(0).getPrice();
+	            
+	            if(order_type.equals("MARKET"))
+	            	query="select * from ORDERS where SIDE='" + other_type + "' and PAIR='" + pair + "' and QTY=" + quantity;
+	            else
+	            	{
+		            	if(type.equals("BUY"))
+		            		query="select * from ORDERS where SIDE='" + other_type + "' and PAIR='" + pair + "' and QTY=" + quantity + " and PRICE<=" + price;
+		            	else
+		            		query="select * from ORDERS where SIDE='" + other_type + "' and PAIR='" + pair + "' and QTY=" + quantity + " and PRICE>=" + price;
+		            		
+	            	}	
+	            
+	            System.out.println(query);
+	            List<TransactionOrder> list1 = new ArrayList<TransactionOrder>();
+	            list1=jdbcTemplate.query(query, new UserMapper());
+	            int size=list1.size();
+	            System.out.println(size);
+
+
+	if(user_exists(order.getUsername(),order.getPassword()))
+	{
+		System.out.println("Authenticated");
+	    if(size>0)
+	    {
+	        
+	        	System.out.println("Size is 1");
+	            int id=list1.get(0).getOid();
+	            String name= list1.get(0).getUsername();
+	            System.out.println(name);
+	            if(buyer== null){
+	            	
+	                buyer=name;
+	                System.out.println("Buyer is " + buyer);
+	            }
+	            else
+	            {
+		            seller=name;
+	            }
+	            if(order_type.equals("MARKET")){
+		            
+		            String query1= "insert into SUC_TRANSACTION(BUYER, SELLER,PAIR, PRICE, QTY,TIME_STAMP) values (?,?,?,?,?,?,)";
+		            jdbcTemplate.update(query1, buyer, seller,order.getPair(), market_price, order.getQuantity(), timeStamp);
+		            
+		            jdbcTemplate.update("delete from ORDERS where OID=?",id);
+	            }
+	            else
+	            {
+	            	String query1= "insert into SUC_TRANSACTION(BUYER, SELLER,PAIR, PRICE, QTY,TIME_STAMP) values (?,?,?,?,?,?,)";
+		            jdbcTemplate.update(query1, buyer, seller,order.getPair(), order.getPrice(), order.getQuantity(), timeStamp);
+		            
+		            jdbcTemplate.update("delete from ORDERS where OID=?",id);
+	            }
+
+	           
+	        }
+	        
+	    else
+	    {
+	    	if(order_type.equals("MARKET"))
+		        jdbcTemplate.update("insert into ORDERS(USERNAME, ORDER_TYPE,PRICE, QTY, PAIR, SIDE, TIME_STAMP,) values (?,?,?,?,?,?,?)",
+		        order.getUsername(), order.getOrder_type(), market_price,order.getQuantity(),order.getPair(), order.getSide(), timeStamp);
+	    	else
+	    		jdbcTemplate.update("insert into ORDERS(USERNAME, ORDER_TYPE,PRICE, QTY, PAIR, SIDE, TIME_STAMP,) values (?,?,?,?,?,?,?)",
+	    		order.getUsername(), order.getOrder_type(), order.getPrice(),order.getQuantity(),order.getPair(), order.getSide(), timeStamp);
+	    }
+	}
+	       return order;
+
+	        }
+
+	    	
+//if(user_exists(order.getUsername(),order.getPassword()))
+//	    	jdbcTemplate.update("insert into ORDERS(USERNAME, ORDER_TYPE, PRICE, QTY, PAIR, SIDE, TIME_STAMP,) values (?,?,?,?,?,?,?)", 
+//	    order.getUsername(), order.getOrder_type(), order.getPrice(), order.getQuantity(),order.getPair(), order.getSide(), timeStamp);
+//	        
+//	   return order;
+//	    }
 
 //	    @Transactional(readOnly=true)
 //	    public List<TransactionOrder> findAllOrders() {
@@ -120,6 +219,20 @@ class OrderRowMapper implements RowMapper<TransactionOrder>
 		return order;
 	}
 }
+class UserMapper implements RowMapper<TransactionOrder>
+
+{
+     @Override
+    public TransactionOrder mapRow(ResultSet rs, int rowNum) throws
+SQLException {
+       TransactionOrder order = new TransactionOrder();
+
+       order.setOid(rs.getInt("OID"));
+       order.setUsername(rs.getString("USERNAME"));
+
+      return order;
+     }
+}
 //{
 //    "username": "SAYLI",
 //    "order_type": "market",
@@ -131,6 +244,14 @@ class OrderRowMapper implements RowMapper<TransactionOrder>
 //}
 
 
-		
+class UserRow implements RowMapper<OpenClosedOrders>
 
+{
+     @Override
+    public OpenClosedOrders mapRow(ResultSet rs, int rowNum) throws SQLException {
+       OpenClosedOrders oco = new OpenClosedOrders();
+       oco.setPrice(rs.getDouble("PRICE"));
+       return oco;
+     }
+}
 
